460a5671e78e67e0425c431b72e11e8677b0503c
Donald Newholm <newholm1@hotmail.com>
2019-12-10 23:28:13 +0000

    User can sign up and receive a welcome message.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   end
-  
+
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirm)
+    end
+
 end
