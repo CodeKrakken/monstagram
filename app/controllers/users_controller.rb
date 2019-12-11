749baf66b1071dec377910b2a54529e839538b0d
Student <student@Makerss-MBP.connect>
2019-12-11 15:54:12 +0000

    Added page that displays all users with link to their welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -16,7 +19,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirm)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :confirm_password)
     end
 
 end
