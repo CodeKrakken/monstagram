5f5fb939c6201ed37924f2d35c88a226ededf534
CodeKrakken <newholm1@hotmail.com>
2020-01-27 16:58:09 +0000

    Cleaning up commit history

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :timeoutable
   validates :first_name, presence: true, length: { minimum: 1 }
   # validates: last_name, presence: true, length: { minimum: 1 }
+  validates :password, confirmation: true
 end
--- db/migrate/20191210225805_create_users.rb
+++ db/migrate/20191210225805_create_users.rb
@@ -1,11 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :bums do |t|
+    create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
-      t.string :password
-      t.string :confirm_password
 
       t.timestamps
     end
